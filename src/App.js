4287c72f76bd8715596f94f55c40907d1769e4b0
Frances <frances.ferrer1999@gmail.com>
2022-08-29 18:49:06 +0200

    game logic updated

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ function App() {
   const [dices, setDices] = useState(createDices())
   const [isWon, setIsWon] = useState(false) //state to check if game is won
   const [moves, setMoves] = useState(0) //state to check number of moves taken to win a game
+  const [bestScore, setBestSCore] = useState(localStorage.getItem("best"))
+
+  useEffect(() => {
+    checkWin()
+  }, [dices])
 
   function createDices(){
     const diceArray = []
     for(let i=0; i < difficulty; i++){
-      console.log("dice created")
       diceArray[i] = createDice()
     }
     return diceArray
@@ -27,21 +31,56 @@ function App() {
 
   function buttonHandler(){
     setMoves(prevMoves => prevMoves+1)
-    //roll new dices if dice is not held
+    if(isWon){
+      setMoves(0)
+      setDices(createDices())
+      setIsWon(false)
+    }
+    else setDices(prevDices => {
+      const newDices = prevDices.map((dice)=>{
+        return !dice.isHeld ? createDice() : dice
+      })
+      return newDices
+    })
   }
 
-  const playDices = dices.map((dice) =>{
-    return <Dice key={dice.id} value={dice.value}/>
+  function toggleIsHeld(id){
+    setDices(prevDices => {
+      const newDices = prevDices.map((dice)=>{
+        return dice.id === id ? {...dice, isHeld: !dice.isHeld} : dice
+      })
+      return newDices
+    })
+  }
+
+  function checkWin(){
+    const allHeld = dices.every(dice => dice.isHeld)
+    const firstValue = dices[0].value
+    const allSameValue = dices.every(dice => dice.value === firstValue)
+    if (allHeld && allSameValue)
+      setIsWon(true)
+  }
+
+  function saveGameStats(){
+    setBestSCore(moves)
+    localStorage.setItem("best", moves)
+  }
+
+  const playDices = dices.map(dice =>{
+    return <Dice key={dice.id} value={dice.value} isHeld={dice.isHeld} id={dice.id} toggleIsHeld={toggleIsHeld}/>
   })
 
   return (
     <div className="App">
       <div className="game">
         <h1>Tenzies</h1>
+        <h3>Best Score: {bestScore}</h3>
+        <h3>Current moves: {moves}</h3>
         <div className="container">
           {playDices}
         </div>
-      <button className="btn" onClick={buttonHandler}>hi</button>
+      <button className="btn" onClick={buttonHandler}>{isWon ? "New Game" : "Reroll"}</button>
+      {isWon && <button className="btn" onClick={saveGameStats}>Save Game</button>}
       </div>
     </div>
   );
--- src/Components/dice.js
+++ src/Components/dice.js
@@ -1,8 +1,12 @@
 import React from "react"
 
 function Dice(props){
+    const styling={
+        backgroundColor: props.isHeld ? "#6ff77f" : "white"
+    }
+
     return(
-        <div className="dice">
+        <div className="dice" onClick={()=>props.toggleIsHeld(props.id)} style={styling}>
             <h1 className="dice-value">
                 {props.value}
             </h1>
